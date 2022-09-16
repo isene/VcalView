8ba2bcd9974c2c1a850fb917af69cf6dc0bf40bd
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2022-09-16 10:34:49 +0300

    Use the TZID values if provided

--- calview.rb
+++ calview.rb
@@ -17,10 +17,15 @@ vcal.gsub!( /(^ATTENDEE.*)\n^ (.*)/, '\1\2' )
 
 # Get dates and times
 if vcal.match( /^DTSTART;TZID=/ )
+  require "tzinfo"
   sdate = vcal[ /^DTSTART;TZID=.*:(.*)T/, 1 ].sub( /(\d\d\d\d)(\d\d)(\d\d)/, '\1-\2-\3') 
   edate = vcal[ /^DTEND;TZID=.*:(.*)T/, 1 ].sub( /(\d\d\d\d)(\d\d)(\d\d)/, '\1-\2-\3') 
   stime = vcal[ /^DTSTART;TZID=.*T(\d\d\d\d)/, 1 ].sub( /(\d\d)(\d\d)/, '\1:\2')
   etime = vcal[ /^DTEND;TZID=.*T(\d\d\d\d)/, 1 ].sub( /(\d\d)(\d\d)/, '\1:\2')
+  stz = vcal[ /^DTSTART;TZID=(.*):/, 1 ]
+  etz = vcal[ /^DTEND;TZID=(.*):/, 1 ]
+  stz = TZInfo::Timezone.get(stz)
+  etz = TZInfo::Timezone.get(etz)
 elsif vcal.match( /DTSTART;VALUE=DATE:/ )
   sdate = vcal[ /^DTSTART;VALUE=DATE:(.*)/, 1 ].sub( /(\d\d\d\d)(\d\d)(\d\d)/, '\1-\2-\3') 
   edate = vcal[ /^DTEND;VALUE=DATE:(.*)/, 1 ].sub( /(\d\d\d\d)(\d\d)(\d\d)/, '\1-\2-\3') 
@@ -34,11 +39,15 @@ elsif vcal.match( /DTSTART;VALUE=DATE:/ )
   rescue
     etime = stime
   end
+  stz = nil
+  etz = nil
 else
   sdate = vcal[ /^DTSTART:(.*)T/, 1 ].sub( /(\d\d\d\d)(\d\d)(\d\d)/, '\1-\2-\3') 
   edate = vcal[ /^DTEND:(.*)T/, 1 ].sub( /(\d\d\d\d)(\d\d)(\d\d)/, '\1-\2-\3') 
   stime = vcal[ /^DTSTART.*T(\d\d\d\d)/, 1 ].sub( /(\d\d)(\d\d)/, '\1:\2')
   etime = vcal[ /^DTEND.*T(\d\d\d\d)/, 1 ].sub( /(\d\d)(\d\d)/, '\1:\2')
+  stz = nil
+  etz = nil
 end
 
 # Adjust for local TZ offset
@@ -69,12 +78,14 @@ else
   end
 end
 
+if stz then stime = stz.local_to_utc(Time.parse(sdate + " " + stime)); stime = stime.localtime end
+if etz then etime = etz.local_to_utc(Time.parse(edate + " " + etime)); etime = etime.localtime end
+
 sdate == edate ? dates = sdate : dates = sdate + " - " + edate
 dobj = Time.parse( sdate )
 wday = dobj.strftime('%A')
 week = dobj.strftime('%-V')
-stime == etime ? times = stime : times = stime + " - " + etime
-times += " (GST)" if vcal.match( /DTSTART;TZID=Greenwich Standard Time/ )
+stime == etime ? times = stime : times = stime.strftime("%H:%M") + " - " + etime.strftime("%H:%M")
 # Get participants
 part  = vcal.scan( /^ATTENDEE.*CN=([\s\S]*?@.*)\n/ ).join('%').gsub( /\n /, '').gsub( /%/, ">\n   " ).gsub( /:mailto:/i, " <" )
 part  = "   " + part + ">" if part != ""
